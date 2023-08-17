ef4ae68e0ca65a8cec6454ea103b29cda2f7ca38
JuanseFisaTS <142258092+JuanseFisaTS@users.noreply.github.com>
2023-08-17 14:57:31 -0500

    Caluladora de notas

---
+++ Grades/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+	
+	static Student stu = new Student(95);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(stu.assignGrade());
+
+	}
+
+}
